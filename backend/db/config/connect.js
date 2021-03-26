ec65a59298605c4f8a0288d5d17146ef326def06
Alex Dodd <alexdodd@gmail.com>
2021-03-26 23:51:55 +0000

    ESLint, prettier, react-app-boiler

---
+++ backend/db/config/connect.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connect = async () => {
+	try {
+		await mongoose.connect(process.env.MONGODB, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+
+		console.log('MongoDB connected');
+	} catch (error) {
+		console.error('MongoDB connection failure');
+		process.exit(1);
+	}
+};
+
+module.exports = connect;
---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+	return <div className='App'>Front End.</div>;
+}
+
+export default App;
