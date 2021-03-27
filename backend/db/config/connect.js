29266bc27b20bf59c8fdb5abdf2c56e2ea87396c
Alex Dodd <alexdodd@gmail.com>
2021-03-27 00:24:25 +0000

    mongoDB connection boiler with mongoose

--- backend/db/config/connect.js
+++ backend/db/config/connect.js
@@ -2,17 +2,18 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connect = async () => {
-	try {
-		await mongoose.connect(process.env.MONGODB, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+  try {
+    await mongoose.connect(process.env.MONGODB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
 
-		console.log('MongoDB connected');
-	} catch (error) {
-		console.error('MongoDB connection failure');
-		process.exit(1);
-	}
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection failure');
+    process.exit(1);
+  }
 };
 
 module.exports = connect;
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const connect = require('./db/config/connect.js');
+
+connect();
 
 const app = express();
 app.use(express.json());
 const SERVER_PORT = process.env.SERVER_PORT || 4000;
 
 app.listen(SERVER_PORT, () =>
-	console.log(`Server listening on ${SERVER_PORT}`)
+  console.log(`Server listening on ${SERVER_PORT}`),
 );
