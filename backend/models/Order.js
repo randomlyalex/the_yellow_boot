5b9162e0b5c91b1ed5be4d577d243be79ef59eaf
Alex Dodd <alexdodd@gmail.com>
2021-03-27 01:49:59 +0000

    created order model

---
+++ backend/models/Order.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const OrderSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+  },
+  items: [
+    {
+      productId: {
+        type: String,
+      },
+      name: String,
+      quantity: {
+        type: Number,
+        required: true,
+        min: [1, 'Quantity can not be less then 1.'],
+        default: 1,
+      },
+      price: Number,
+    },
+  ],
+  total: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  date_ordered: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Order = mongoose.model('order', OrderSchema);
+
+module.exports = Order;
