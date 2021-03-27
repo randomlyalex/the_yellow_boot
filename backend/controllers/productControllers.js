90b0194975ce4519265799ebf056b0ca2833bb50
Alex Dodd <alexdodd@gmail.com>
2021-03-27 23:57:52 +0000

    re-writing the fetches to async await

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,28 +1,68 @@
 const Product = require('../models/Product');
 
-module.exports.get_products = (req, res) => {
-  Product.find()
-    .sort({ date: -1 })
-    .then((products) => res.json(products));
+const getProducts = async (req, res) => {
+  try {
+    //double check this works with insomnia https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await
+    const products = await Product.find()
+      .sort({ date: -1 })
+      .then((products) => res.json(products));
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
 };
 
-module.exports.post_product = (req, res) => {
-  const newProduct = new Product(req.body);
-  newProduct.save().then((product) => res.json(product));
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
 };
 
-module.exports.update_product = (req, res) => {
-  Product.findByIdAndUpdate({ _id: req.params.id }, req.body).then(
-    (product) => {
-      Product.findOne({ _id: req.params.id }).then((product) => {
-        res.json(product);
-      });
-    }
-  );
+const postProduct = async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    newProduct.save().then((product) => res.json(product));
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
 };
 
-module.exports.delete_product = (req, res) => {
-  Product.findByIdAndDelete({ _id: req.params.id }).then((product) => {
-    res.json({ success: true });
-  });
+const updateProduct = async (req, res) => {
+  try {
+    Product.findByIdAndUpdate({ _id: req.params.id }, req.body).then(
+      (product) => {
+        Product.findOne({ _id: req.params.id }).then((product) => {
+          res.json(product);
+        });
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    Product.findByIdAndDelete({ _id: req.params.id }).then((product) => {
+      res.json({ success: true });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+module.exports = {
+  getProducts,
+  getProductById,
+  postProduct,
+  updateProduct,
+  deleteProduct,
 };
