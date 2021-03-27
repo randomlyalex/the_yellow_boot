0e3e56e75c8ad518f4eb2884372c987cbcd9f63a
Alex Dodd <alexdodd@gmail.com>
2021-03-27 23:33:09 +0000

    test seed data

--- backend/db/data/userData.js
+++ backend/db/data/userData.js
@@ -1,3 +1,22 @@
-const users = {};
+const users = [
+  {
+    name: 'User 1',
+    email: 'user1@thewebsite.com',
+    emailValidated: true,
+    password: 'veryStrongPassword',
+  },
+  {
+    name: 'User 2',
+    email: 'user2@thewebsite.com',
+    emailValidated: true,
+    password: 'veryStrongPassword',
+  },
+  {
+    name: 'User 3',
+    email: 'user3@thewebsite.com',
+    emailValidated: true,
+    password: 'veryStrongPassword',
+  },
+];
 
 module.exports = users;
