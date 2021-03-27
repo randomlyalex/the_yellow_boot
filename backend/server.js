01aea46f4f892f203fa3752124dfbdbbe6f8b962
Alex Dodd <alexdodd@gmail.com>
2021-03-27 23:58:33 +0000

    add routes to server.js

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const connect = require('./db/config/connect.js');
 
+const authRoutes = require('./routes/authRouter');
+const productRoutes = require('./routes/productRouter');
+
 connect();
 
 const app = express();
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.json({ message: 'API backend running...' });
+});
+
+app.use('/api', authRoutes);
+app.use('/api', productRoutes);
+
 const SERVER_PORT = process.env.SERVER_PORT || 4000;
 
 app.listen(SERVER_PORT, () =>
