cf06fbe56da52042719e6c82417514e33c57449b
Alex Dodd <alexdodd@gmail.com>
2021-03-28 00:01:47 +0000

    Router changed to Routes for clarity

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const connect = require('./db/config/connect.js');
 
-const authRoutes = require('./routes/authRouter');
-const productRoutes = require('./routes/productRouter');
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
 
 connect();
 
 const app = express();
 app.use(express.json());
+
 app.get('/', (req, res) => {
   res.json({ message: 'API backend running...' });
 });
@@ -18,7 +19,6 @@ app.use('/api', authRoutes);
 app.use('/api', productRoutes);
 
 const SERVER_PORT = process.env.SERVER_PORT || 4000;
-
 app.listen(SERVER_PORT, () =>
   console.log(`Server listening on ${SERVER_PORT}`)
 );
