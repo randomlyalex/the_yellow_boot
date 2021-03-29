9b76659ed8966c917aa76fc90e6415ab8014be5e
Alex Dodd <alexdodd@gmail.com>
2021-03-29 01:58:11 +0100

    fixed my route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
-const productController = require('../controllers/itemControllers');
+const productController = require('../controllers/productControllers');
 const router = Router();
 
 router.get('/products', productController.getProducts);
-router.get('/products:id', productController.getProductById);
+router.get('/products/:id', productController.getProductById);
 router.post('/products', productController.postProduct);
 router.put('/products/:id', productController.updateProduct);
 router.delete('/products/:id', productController.deleteProduct);
