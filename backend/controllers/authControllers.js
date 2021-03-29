896f450e02c4f45fefe9e4b48e9b6b10add96bdd
Alex Dodd <alexdodd@gmail.com>
2021-03-29 04:05:46 +0100

    started to refactor thens to asyncs with bcrypt

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -3,43 +3,44 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-const register = (req, res) => {
-  // this needs async await
+const register = async (req, res) => {
   const { username, name, email, password } = req.body;
 
   if (!username || !name || !email || !password) {
     res.status(400).json({ msg: 'Please enter all fields' });
   }
 
-  User.findOne({ email }).then((user) => {
-    if (user) return res.status(400).json({ msg: 'Email already in use.' });
+  const user = await User.findOne({ email });
 
-    const newUser = new User({ username, name, email, password });
+  if (user) return res.status(400).json({ msg: 'Email already in use.' });
 
-    // Create salt and hash
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser.save().then((user) => {
-          jwt.sign(
-            { id: user._id },
-            process.env.JWT_SECRET,
-            { expiresIn: 3600 },
-            (err, token) => {
-              if (err) throw err;
-              res.json({
-                token,
-                user: {
-                  id: user._id,
-                  user: user.username,
-                  name: user.name,
-                  email: user.email,
-                },
-              });
-            }
-          );
-        });
+  const newUser = new User({ username, name, email, password });
+
+  //fix this with async? https://attacomsian.com/blog/nodejs-password-hashing-with-bcrypt
+  // https://www.npmjs.com/package/bcrypt#api
+  // Create salt and hash
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser.save().then((newUser) => {
+        jwt.sign(
+          { id: newUser._id },
+          process.env.JWT_SECRET,
+          { expiresIn: 3600 },
+          (err, token) => {
+            if (err) throw err;
+            res.json({
+              token,
+              newUser: {
+                id: user._id,
+                username: user.username,
+                name: user.name,
+                email: user.email,
+              },
+            });
+          }
+        );
       });
     });
   });
@@ -50,33 +51,33 @@ const login = async (req, res) => {
   if (!password || !(email || username)) {
     res.status(400).json({ msg: 'Username or Email and password required.' });
   }
-  user = User.findOne({
+  const user = await User.findOne({
     $or: [{ email }, { username }],
-  }).then((user) => {
-    //this needs await adding and catches
-    if (!user) return res.status(400).json({ msg: 'Incorrect details' });
+  });
+  //this needs await adding and catches
+  if (!user) return res.status(400).json({ msg: 'Incorrect details' });
 
-    // Validate password
-    bcrypt.compare(password, user.password).then((isMatch) => {
-      if (!isMatch) return res.status(400).json({ msg: 'Login failed' });
+  // Validate password
+  bcrypt.compare(password, user.password).then((isMatch) => {
+    if (!isMatch) return res.status(400).json({ msg: 'Login failed' });
 
-      jwt.sign(
-        { id: user._id },
-        process.env.JWT_SECRET,
-        { expiresIn: 3600 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({
-            token,
-            user: {
-              id: user._id,
-              name: user.name,
-              email: user.email,
-            },
-          });
-        }
-      );
-    });
+    jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: 3600 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({
+          token,
+          user: {
+            id: user._id,
+            username: user.username,
+            name: user.name,
+            email: user.email,
+          },
+        });
+      }
+    );
   });
 };
 
