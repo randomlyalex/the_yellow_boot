e593792e9bb709d86c55b52ce29c3a020dbc840a
Alex Dodd <alexdodd@gmail.com>
2021-03-29 20:40:12 +0100

    basket needs to store same number trio as products

--- backend/models/Basket.js
+++ backend/models/Basket.js
@@ -16,17 +16,23 @@ const BasketSchema = new mongoose.Schema({
         min: [1, 'Quantity can not be less then 1.'],
         deafult: 1,
       },
-      price: Number,
+      price: [Number],
     },
   ],
   total: {
-    type: Number,
+    type: [Number],
     required: true,
     default: 0,
   },
   date_created: {
     type: Date,
     default: Date.now,
+  },
+  date_updated: {
+    date_created: {
+      type: Date,
+      default: Date.now,
+    },
   },
 });
 
