69fb8025bdbe0946ae52abf64e0b3afa7a4d5907
Alex Dodd <alexdodd@gmail.com>
2021-03-30 00:19:25 +0100

    simplified and made the query more predictable

--- backend/routes/basketRoutes.js
+++ backend/routes/basketRoutes.js
@@ -2,8 +2,8 @@ const { Router } = require('express');
 const basketController = require('../controllers/basketControllers');
 const router = Router();
 
-router.get('/basket/:id', basketController.getProducts);
-router.post('/basket/:id', basketController.addProduct);
-router.delete('/basket/:userId/:itemId', basketController.removeProduct);
+router.get('/basket/:query', basketController.getProducts);
+router.post('/basket/:query', basketController.addProduct);
+router.delete('/basket/:query', basketController.removeProduct);
 
 module.exports = router;
