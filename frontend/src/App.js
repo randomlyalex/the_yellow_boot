406882f61cb5ba68920d09bbea268d50b7a6f35b
Alex Dodd <alexdodd@gmail.com>
2021-04-03 01:46:19 +0100

    Added main container components to App for UI test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,54 @@
 import './App.css';
 import { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  withRouter,
+} from 'react-router-dom';
 //Components
 import HeaderBar from './components/HeaderBar';
 import Navigation from './components/Navigation';
+import HomeContainer from './containers/HomeContainer';
+import ResultsContainer from './containers/ResultsContainer';
+import UserAccountContainer from './containers/UserAccountContainer';
+import StoreLocator from './containers/StoreLocator';
+import BasketContainer from './containers/BasketContainer';
+import FooterBar from './components/FooterBar';
+import HelpContainer from './containers/HelpContainer';
+import ProductDetail from './containers/ProductDetail';
 
 //Containers
 
 function App() {
   return (
     <Router>
-      <HeaderBar></HeaderBar>
-      <Navigation></Navigation>
+      <header>
+        <HeaderBar />
+      </header>
+      <Navigation />
+      <main>
+        <Switch>
+          <Route exact path="/" render={() => <HomeContainer />} />
+          <Route path="/womens" render={() => <ResultsContainer />} />
+          <Route path="/mens" render={() => <ResultsContainer />} />
+          <Route path="/kids" render={() => <ResultsContainer />} />
+          <Route path="/sale" render={() => <ResultsContainer />} />
+          <Route path="/basket" render={() => <BasketContainer />} />
+          <Route exact path="/product/:id" render={() => <ProductDetail />} />
+          <Route path="/my-account" render={() => <UserAccountContainer />} />
+          <Route path="/help" render={() => <HelpContainer />} />
+          <Route path="/stores" render={() => <StoreLocator />} />
+          <Route exact path="/404" render={() => <p>404 page not found</p>} />
+          <Redirect to="/404" />
+
+          <Redirect to="/404" />
+        </Switch>
+      </main>
+      <footer>
+        <FooterBar />
+      </footer>
       {/* Top Navbar - Desktop */}
       {/* SideDrawer - Mobile */}
       {/* HomeScreen */}
