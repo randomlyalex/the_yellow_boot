7b24ffeba4ee4b70bf8e08fc1d35f9b99ce2a1cd
Alex Dodd <alexdodd@gmail.com>
2021-04-05 14:21:06 +0100

    logic and routes associated with auth
    signin, signup and account details etc...

--- frontend/src/containers/userAccountContainer.js
+++ frontend/src/containers/userAccountContainer.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  withRouter,
+} from 'react-router-dom';
+
+//MaterialUI
+import Button from '@material-ui/core/Button';
+
+//Components
+import SignIn from '../components/SignIn';
+import SignUp from '../components/SignUp';
+
+//Actions
+import { logout } from '../redux/actions/authActions';
 
 const UserAccountContainer = () => {
-  return <div>My Account Section</div>;
+  const dispatch = useDispatch();
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <>
+      {!isAuthenticated ? (
+        <Switch>
+          <Route exact path="/my-account/sign-up" render={() => <SignUp />} />
+          <Route path="/my-account" render={() => <SignIn />} />
+        </Switch>
+      ) : (
+        'You are logged in'
+      )}
+    </>
+  );
 };
 
 export default UserAccountContainer;
