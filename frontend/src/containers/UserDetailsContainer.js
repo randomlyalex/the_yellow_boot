75cf22bcc4d54cc11c21efa12d2db8afc2441f1f
Alex Dodd <alexdodd@gmail.com>
2021-04-05 20:11:50 +0100

    beginning of user control area

---
+++ frontend/src/containers/UserDetailsContainer.js
@@ -0,0 +1,25 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+//Actions
+import { getOrders as listOrders } from '../redux/actions/orderActions';
+
+const UserDetailsContainer = () => {
+  const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.auth.user);
+
+  return (
+    <div>
+      <p>Address' can be added and removed from here.</p>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <p>Username: {user.username}</p>
+      <p> Change password link</p>
+    </div>
+  );
+};
+
+export default UserDetailsContainer;
