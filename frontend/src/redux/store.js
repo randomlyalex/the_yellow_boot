e3d93830ba077d34d4bdb3565fd1f003e4bd5cd8
Alex Dodd <alexdodd@gmail.com>
2021-04-05 03:22:14 +0100

    combinReducers added to store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,12 +2,32 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducer = combineReducers({});
+//Reducers
+import { authReducers } from './reducers/authReducers';
+import { basketReducers } from './reducers/basketReducers';
+import {
+  productReducers,
+  productDetailReducers,
+} from './reducers/productReducer';
+import { errorReducers } from './reducers/errorReducers';
+import { orderReducers } from './reducers/orderReducers';
+
+const reducer = combineReducers({
+  auth: authReducers,
+  basket: basketReducers,
+  error: errorReducers,
+  getProducts: productReducers,
+  productDetail: productDetailReducers,
+  order: orderReducers,
+});
 
 const middleware = [thunk];
 
+const initialState = {};
+
 const store = createStore(
   reducer,
+  initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
