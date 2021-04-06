a45f5c29ed4ba819dbd19b2241bde11dca03232a
Alex Dodd <alexdodd@gmail.com>
2021-04-06 11:31:57 +0100

    add to basket straight from card with size choice

--- frontend/src/components/productCard.js
+++ frontend/src/components/productCard.js
@@ -1,5 +1,7 @@
 // import { Link } from 'react-router-dom';
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 // Material UI components
 
@@ -12,7 +14,11 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
-import image1 from '../static/img/1.jpg';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Select from '@material-ui/core/Select';
+
+import { addToBasket } from '../redux/actions/basketActions';
 
 const ProductCard = ({ product }) => {
   // const product = {
@@ -33,32 +39,50 @@ const ProductCard = ({ product }) => {
   //   date_added: { $date: '2021-03-27T23:21:01.341Z' },
   //   __v: 0,
   // };
+  const dispatch = useDispatch();
+  const { isAuthenticated, user, isLoading: userLoading } = useSelector(
+    (state) => state.auth
+  );
+
+  const handleAddToBasket = () => {
+    isAuthenticated && dispatch(addToBasket(user._id, product._id, 1));
+  };
 
   return (
     <>
       <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
         <Card>
-          <CardMedia component="img" alt="Shoe 1" height="270" image={image1} />
+          <CardActions>
+            <Link href={`/product/detail/${product._id}`} color="inherit">
+              <CardMedia
+                component="img"
+                alt="Shoe 1"
+                height="270"
+                image={product.imageUrls[0]}
+              />
+            </Link>
+          </CardActions>
           <CardContent>
             <Grid container style={{ justifyContent: 'center' }} wrap="nowrap">
               <Grid item zeroMinWidth>
                 <Typography gutterBottom variant="h5" noWrap>
-                  Name: {product['name']}
+                  {product['name']}
                 </Typography>
               </Grid>
             </Grid>
             <Grid container style={{ justifyContent: 'center' }} wrap="nowrap">
               <Grid item zeroMinWidth>
                 <Typography gutterBottom variant="h5" noWrap>
-                  Brand
+                  {/* Brand */}
                 </Typography>
               </Grid>
             </Grid>
             <br />
             <Typography gutterBottom variant="h4">
-              {'price' in product && product.price[2]}
+              £{'price' in product && product.price[2].toFixed(2)}
             </Typography>
           </CardContent>
+
           <CardActions>
             {/* <Button color="primary">Add to cart</Button> */}
             <Button color="primary">
@@ -66,6 +90,30 @@ const ProductCard = ({ product }) => {
                 More Details
               </Link>
             </Button>
+            {isAuthenticated ? (
+              <>
+                <Button onClick={handleAddToBasket}>Add to Basket</Button>
+                <FormControl>
+                  <FormLabel>Size?</FormLabel>
+                  <Select native>
+                    <option aria-label="None" value=""></option>
+                    <option value={'1'}>1</option>
+                    <option value={'2'}>2</option>
+                    <option value={'3'}>3</option>
+                    <option value={'4'}>4</option>
+                    <option value={'5'}>5</option>
+                    <option value={'6'}>6</option>
+                    <option value={'7'}>7</option>
+                    <option value={'8'}>8</option>
+                    <option value={'9'}>9</option>
+                    <option value={'10'}>10</option>
+                    <option value={'11'}>11</option>
+                  </Select>
+                </FormControl>
+              </>
+            ) : (
+              <Button disabled>Add to Basket</Button>
+            )}
           </CardActions>
         </Card>
       </Grid>
