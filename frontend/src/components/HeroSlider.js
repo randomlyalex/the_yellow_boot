c9b52e5335ace8c686f7c64544f8d4524d592dd8
Alex Dodd <alexdodd@gmail.com>
2021-04-06 11:26:06 +0100

    idea of what the hero slider would look like

--- frontend/src/components/HeroSlider.js
+++ frontend/src/components/HeroSlider.js
@@ -1,7 +1,22 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
 
 const HeroSlider = () => {
-  return <div>This will be a Hero Slider</div>;
+  return (
+    <div>
+      <Grid container justify="center">
+        <Grid item>
+          <img src="/public/img/front2.png" />
+        </Grid>
+        <Grid item>
+          <img src="/public/img/front1.png" />
+        </Grid>
+        <Grid item>
+          <img src="/public/img/front3.png" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default HeroSlider;
