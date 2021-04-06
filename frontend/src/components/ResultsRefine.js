f900b81549b4650b829c523c5a92c2461544a0f2
Alex Dodd <alexdodd@gmail.com>
2021-04-06 11:42:51 +0100

    TBC on refine section

--- frontend/src/components/ResultsRefine.js
+++ frontend/src/components/ResultsRefine.js
@@ -1,7 +1,16 @@
+import { Chip } from '@material-ui/core';
 import React from 'react';
 
 const ResultsRefine = () => {
-  return <aside>This will refine results.</aside>;
+  return (
+    <aside>
+      This will be where a refine aside and top bar UI will be
+      {/* <Chip color="secondary" label="Tag 1" />
+      <Chip color="secondary" label="Example Tags" />
+      <Chip color="secondary" label="Tag 2" />
+      <Chip color="secondary" label="Size 5" /> */}
+    </aside>
+  );
 };
 
 export default ResultsRefine;
