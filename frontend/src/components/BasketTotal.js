4000072b1653013add37bc4afd521f3ace7a9ecb
Alex Dodd <alexdodd@gmail.com>
2021-04-06 13:37:40 +0100

    added the discounts hidden accordian

--- frontend/src/components/BasketTotal.js
+++ frontend/src/components/BasketTotal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 // Redux
 import { useDispatch } from 'react-redux';
@@ -7,12 +8,35 @@ import { useDispatch } from 'react-redux';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import { Grid, Paper, Button } from '@material-ui/core';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from '@material-ui/core/styles';
 
 //Actions
 import { createOrder } from '../redux/actions/orderActions';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+}));
+
 const BasketTotal = ({ basket }) => {
+  const classes = useStyles();
   const dispatch = useDispatch();
+  const [expanded, setExpanded] = useState(false);
 
   const handleCheckout = () => {
     dispatch(createOrder(basket.userId));
@@ -38,7 +62,22 @@ const BasketTotal = ({ basket }) => {
         </Grid>
         <Grid container spacing={5}>
           <Grid item xs={12}>
-            Discount Section
+            <Accordion
+              expanded={expanded}
+              key={1}
+              onChange={() => setExpanded(!expanded)}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1bh-content"
+                id="panel1bh-header">
+                <Typography className={classes.heading}>
+                  Have a code?
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>Discount section could live here</Typography>
+              </AccordionDetails>
+            </Accordion>
           </Grid>
         </Grid>
         <Grid container spacing={2} justify="space-between">
