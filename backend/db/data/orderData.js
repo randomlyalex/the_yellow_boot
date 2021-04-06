e71d11c9d24575dd9ddb790448df33ce8ade5e58
Alex Dodd <alexdodd@gmail.com>
2021-04-06 11:25:08 +0100

    seed create written but no tested imported yet

--- backend/db/data/orderData.js
+++ backend/db/data/orderData.js
@@ -1,3 +1,194 @@
-const orders = {};
+const orders = [
+  {
+    total: 137.97,
+    userId: '606c1d554b0e6140419ac246',
+    items: [
+      {
+        quantity: 1,
+        price: [10, 8, 49.99],
+
+        productId: '606c1d554b0e6140419ac240',
+        name: 'Resposto',
+      },
+      {
+        quantity: 2,
+        price: [10, 8, 43.99],
+
+        productId: '606c1d554b0e6140419ac23f',
+        name: 'Star Player',
+      },
+    ],
+    date_created: {
+      $date: '2021-04-06T08:40:41.881Z',
+    },
+  },
+  {
+    total: 152.97,
+    userId: '606c1d554b0e6140419ac246',
+    items: [
+      {
+        quantity: 1,
+        price: [10, 8, 49.99],
+        _id: {
+          $oid: '606c1f54036dfb412b265260',
+        },
+        productId: '606c1d554b0e6140419ac240',
+        name: 'Resposto',
+      },
+      {
+        quantity: 1,
+        price: [5, 2, 22.99],
+
+        productId: '606c1d554b0e6140419ac241',
+        name: 'Air Presto',
+      },
+      {
+        quantity: 1,
+        price: [5, 2, 79.99],
+
+        productId: '606c1d554b0e6140419ac243',
+        name: 'Dr Martens Glitter',
+      },
+    ],
+    date_created: {
+      $date: '2021-04-06T08:44:04.234Z',
+    },
+  },
+  {
+    total: 149.97,
+    userId: '606c1d554b0e6140419ac246',
+    items: [
+      {
+        quantity: 3,
+        price: [10, 8, 49.99],
+
+        productId: '606c1d554b0e6140419ac240',
+        name: 'Resposto',
+      },
+    ],
+    date_created: {
+      $date: '2021-04-06T08:44:55.032Z',
+    },
+  },
+  {
+    total: 200.94,
+    userId: '606c1d554b0e6140419ac246',
+    items: [
+      {
+        quantity: 3,
+        price: [10, 8, 43.99],
+        _id: {
+          $oid: '606c1fa4036dfb412b26526c',
+        },
+        productId: '606c1d554b0e6140419ac23f',
+        name: 'Star Player',
+      },
+      {
+        quantity: 3,
+        price: [5, 2, 22.99],
+
+        productId: '606c1d554b0e6140419ac241',
+        name: 'Air Presto',
+      },
+    ],
+    date_created: {
+      $date: '2021-04-06T08:45:24.606Z',
+    },
+  },
+  {
+    total: 87.98,
+    userId: '606c1d554b0e6140419ac246',
+    items: [
+      {
+        quantity: 2,
+        price: [10, 8, 43.99],
+
+        productId: '606c1d554b0e6140419ac23f',
+        name: 'Star Player',
+      },
+    ],
+    date_created: {
+      $date: '2021-04-06T08:49:12.196Z',
+    },
+  },
+  {
+    total: 43.99,
+    userId: '606c1d554b0e6140419ac246',
+    items: [
+      {
+        quantity: 1,
+        price: [10, 8, 43.99],
+
+        productId: '606c1d554b0e6140419ac23f',
+        name: 'Star Player',
+      },
+    ],
+    date_created: {
+      $date: '2021-04-06T08:52:57.085Z',
+    },
+  },
+  {
+    total: 225.95000000000002,
+    userId: '606c1d554b0e6140419ac246',
+    items: [
+      {
+        quantity: 4,
+        price: [10, 8, 43.99],
+
+        productId: '606c1d554b0e6140419ac23f',
+        name: 'Star Player',
+      },
+      {
+        quantity: 1,
+        price: [10, 8, 49.99],
+
+        productId: '606c1d554b0e6140419ac240',
+        name: 'Resposto',
+      },
+    ],
+    date_created: {
+      $date: '2021-04-06T08:54:33.800Z',
+    },
+  },
+  {
+    total: 199.96,
+    userId: '606c1d554b0e6140419ac246',
+    items: [
+      {
+        quantity: 4,
+        price: [10, 8, 49.99],
+
+        productId: '606c1d554b0e6140419ac240',
+        name: 'Resposto',
+      },
+    ],
+    date_created: {
+      $date: '2021-04-06T09:13:15.651Z',
+    },
+  },
+  {
+    total: 133.96,
+    userId: '606c1d554b0e6140419ac246',
+    items: [
+      {
+        quantity: 2,
+        price: [10, 8, 49.99],
+
+        productId: '606c1d554b0e6140419ac240',
+        name: 'Resposto',
+      },
+      {
+        quantity: 2,
+        price: [5, 2, 16.99],
+
+        productId: '606c1d554b0e6140419ac242',
+        name: 'Max Air Bolt',
+      },
+    ],
+    date_created: {
+      $date: '2021-04-06T09:20:35.229Z',
+    },
+  },
+];
 
 module.exports = orders;
