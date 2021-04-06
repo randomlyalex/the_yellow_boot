fa0b24213266231de1901b46ee2333642e0d8016
Alex Dodd <alexdodd@gmail.com>
2021-04-06 03:12:50 +0100

    hooked the basket container to redux stores and actions
    empty basket is suffering from a problem, so is decrement quantity
    
    the API route works, which means there is a problem in frontend logic
    
    most like a problem in my implementation of redux

--- frontend/src/containers/basketContainer.js
+++ frontend/src/containers/basketContainer.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { Button, Grid } from '@material-ui/core';
 import BasketHeader from '../components/BasketHeader';
 import BasketItem from '../components/BasketItem';
 import BasketTotal from '../components/BasketTotal';
+import {
+  getBasket,
+  addToBasket,
+  removeFromBasket,
+  emptyBasket,
+} from '../redux/actions/basketActions';
 
 const BasketContainer = () => {
+  const dispatch = useDispatch();
+
+  const { basketItems: basket, loading: basketLoading } = useSelector(
+    (state) => state.basket
+  );
+
+  const { order } = useSelector((state) => state.order);
+
+  useEffect(() => {
+    dispatch(getBasket());
+  }, [dispatch, order]);
+
+  const handleEmptyBasket = () => {
+    dispatch(emptyBasket(basket.userId, basket._id));
+  };
+
   return (
     <>
       <Grid container justify="center" style={{ paddingTop: '25px' }}>
@@ -12,17 +37,14 @@ const BasketContainer = () => {
         <Grid container item xs={12} sm={8}>
           <Grid item xs={12} md={8}>
             <BasketHeader />
-            <BasketItem />
-            <BasketItem />
-            <BasketItem />
-            <BasketItem />
-            <BasketItem />
-            <BasketItem />
-            <BasketItem />
-            <BasketItem />
+            <Grid item xs={12} md={8}>
+              <Button onClick={handleEmptyBasket}>Empty basket</Button>
+            </Grid>
+            {'items' in basket &&
+              basket.items.map((item) => <BasketItem item={item} />)}
           </Grid>
           <Grid item xs={12} md={4}>
-            <BasketTotal />
+            {'items' in basket && <BasketTotal basket={basket} />}
           </Grid>
         </Grid>
         <Grid item xs={false} sm={2} />
