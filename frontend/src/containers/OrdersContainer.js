146fc46dc659d735f524a65a255dd217bf4fd4c3
Alex Dodd <alexdodd@gmail.com>
2021-04-06 03:13:31 +0100

    loading orders from redux
    putting them into the orderItem components

--- frontend/src/containers/OrdersContainer.js
+++ frontend/src/containers/OrdersContainer.js
@@ -3,26 +3,33 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import OrderDetail from '../components/OrderDetail';
+import OrderItem from '../components/OrderItem';
 
 //Actions
 import { getOrders as listOrders } from '../redux/actions/orderActions';
+import { logout, loadUser } from '../redux/actions/authActions';
 
 const OrdersContainer = () => {
   const [clickedOrder, setClickedOrder] = useState(null);
-
   const dispatch = useDispatch();
-
-  const { id: uid, isLoading: authLoading, isAuthenticated } = useSelector(
-    (state) => state.auth.user
+  const { isAuthenticated, user, isLoading: userLoading } = useSelector(
+    (state) => state.auth
   );
-
   const { orders: userOrders, loading: ordersLoading } = useSelector(
     (state) => state.order
   );
 
   useEffect(() => {
-    dispatch(listOrders(uid));
+    dispatch(loadUser());
   }, [dispatch]);
+
+  useEffect(() => {
+    isAuthenticated && dispatch(listOrders(user._id));
+  }, [isAuthenticated, dispatch, user, userLoading]);
+
+  const handleOrderClick = (order) => {
+    setClickedOrder(order);
+  };
 
   return (
     <>
@@ -31,18 +38,18 @@ const OrdersContainer = () => {
           'Loading orders'
         ) : (
           <>
-            {clickedOrder && <OrderDetail clickedOrder={clickedOrder} />}
-            {userOrders.map((order) => (
-              <>
-                <p
-                  key={order._id}
-                  onClick={() => {
-                    setClickedOrder(order);
-                  }}>
-                  {order.date_ordered} - {order.total}
-                </p>
-              </>
-            ))}
+            <Grid container justify="center">
+              <Grid item>
+                {userOrders.map((order) => (
+                  <div onClick={() => handleOrderClick(order)}>
+                    <OrderItem key={order._id} order={order} />
+                  </div>
+                ))}
+              </Grid>
+              <Grid>
+                {clickedOrder && <OrderDetail clickedOrder={clickedOrder} />}
+              </Grid>
+            </Grid>
           </>
         )}
       </div>
